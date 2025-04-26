41f6f4ba914220b2eb62de16adc1ff85ef9392d6
LuckElixir <sriniranjan.gs@gmail.com>
2025-04-26 16:52:56 -0400

    updated backend and file editing system

--- backend/main.py
+++ backend/main.py
@@ -3,30 +3,56 @@ from enum import Enum
 import math
 import os
 import logging
+from filesave.main import *
 
-Type = Enum('Type', [('Music', 1), ('Math', 2), ('English', 3)])
+class Type:
+    MUSIC = 1
+    MATH = 2
+    ENGLISH = 3
+    COMMUNITY_SERVICE = 4
+class Effort:
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
 
 class Tasks:
     TYPES = []
-    def __init__(self, name: str, type: Type, effort, baseHours, points, base) -> None:
+
+    def __init__(self, name: str, type: Type, effort: Effort, baseHours, points, base) -> None:
         self.name = name
-        self.type = type
+        self.type: Type = type
         self.baseHours = baseHours
         self.effort = effort
+        self.points = self.computePoints()
 
-    
-    def computeXP(self, hours) -> int:
-        XP = None
-        return XP
-        self.points = points
+    def computePoints(self):
+        points = 10
+        if self.effort == Effort.HIGH:
+            points *= 2
+        elif self.effort == Effort.MEDIUM:
+            points *= 1.5
+        else:
+            points = points
+        if self.type == Type.COMMUNITY_SERVICE:
+            points *= 3
+
+    @staticmethod
+    def notify(title, text):
+        os.system("""
+                    osascript -e 'display notification "{}" with title "{}"'
+                    """.format(text, title))
+
+
+class Profile:
+    def __init__(self, name, points=0) -> None:
         self.base = 1.6
-        self.hours: list[float] = []
-        self.completion: list[bool] = []
-        self.activityList = []
-        self.pointsList: list[list] = []
+        self.name: str = name
+        self.rank: str = self.computeRank()
+        self.tasks: list[Tasks] = []
+        self.league = self.computeRank()
+        self.points = points
 
     def computeLevel(self) -> int:
-        self.base = 1.6
         if self.points < 1:
             return 0
         return int(math.log(self.points, self.base) + 0.001 * self.points)
@@ -50,34 +76,5 @@ class Tasks:
             return "Ultimates League"
         return "No League"
 
-    def notify(title, text):
-        os.system("""
-                    osascript -e 'display notification "{}" with title "{}"'
-                    """.format(text, title))
-
-
-
-
-
-
-
-
-    def log(self, time: int, activity: str, completion_input: bool):
-        while 1:
-            self.hours.append(time)
-            self.activityList.append(activity)
-            self.completion.append(completion_input)
-            self.points *= 100
-
-
-
-
-
-class Profile:
-    def __init__(self, name) -> None:
-        self.name : str = name
-        self.tasks : list[Tasks] = []
-
-
-
-
+    def addTask(self, name, ):
+        self.tasks.append(Tasks())
