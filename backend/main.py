ed8bd1d472caac91900b18624e8c4e892ac85d03
TFMao <97853032+TFMao@users.noreply.github.com>
2025-04-26 14:14:32 -0400

    Modify main

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,37 @@
 import ui.main
-import enum.Enum
+from enum import Enum
+import math
 
 Type = Enum('Type', [('Music', 1), ('Math', 2), ('English', 3)
 
 class Tasks:
     TYPES = []
-    def __init__(self, name: str, type: Type, effort, baseHours) -> None:
+    def __init__(self, name: str, type: Type, effort, baseHours, points, base) -> None:
         self.name = name
         self.type = type
         self.baseHours = baseHours
         self.effort = effort
-    
-    def computeXP(self, hours) -> int:
-        XP = 
-        return XP
+        self.points = points
+        self.base = 1.6
+
+    def computeLevel(self) -> int:
+        self.base = 1.6
+        if self.points < 1:
+            return 0
+        return int(math.log(self.points, self.base))
+
+    def computeRank(self) -> str:
+        if self.computeLevel() < 10:
+            return "Bronze League"
+
+
+
+
+
+
 
 class Profile:
     def __init__(self, name) -> None:
         self.name : str = name
         self.tasks : list[Tasks] = []
+        self
